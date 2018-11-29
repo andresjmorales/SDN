0990752e1b54873a5603788aa63e6d6bcd9287c2
moralesandres98 <morales.andres98@gmail.com>
2018-11-28 21:24:33 -0800

    Add files via upload
    This is a working multi-threaded server and client

---
+++ client.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+
+#define SORT_PORT 1719
+#define LINESIZE 80
+int main(int argc,char *argv[]){
+    struct sockaddr_in server;
+    struct hostent *host_info;
+    int sock, count;
+    char inlin[LINESIZE]; /*buffer to copy from user to server*/
+    char outline[LINESIZE + 1];/*buffer to copy from server to user*/
+    char *server_name;
+    /*get server name from command line. if none, use'local host'*/
+    server_name =(argc>1) ? argv[1] :"localhost";
+    /*create the socket*/
+    sock=socket(AF_INET,SOCK_STREAM,0);
+    if(sock<0) {
+        perror("creating stream socket");
+        exit(1);
+    }
+    host_info=gethostbyname(server_name);
+    if(host_info==NULL){
+        fprintf(stderr,"%s:unknown host: %s\n",argv[0],server_name);
+        exit(2);
+    }
+    /*set up the server's socket address, then connect*/
+    server.sin_family=host_info->h_addrtype;
+    memcpy((char *)&server.sin_addr,host_info->h_addr,host_info->h_length);
+    server.sin_port=htons(SORT_PORT);
+    if(connect(sock,(struct sockaddr *)&server,sizeof (server))<0){
+        perror("connecting to server");
+        exit(3);
+    }
+    int wordIndex = 0;
+    printf("\nClient: connect to server %s\n",server_name);
+    printf("Client: Write word %d to sort at server:\n", ++wordIndex);
+    while((count=read(0, outline,LINESIZE))>0){
+        //printf("Client: count = %d\n",count);
+        outline[count]='\0';
+        if(write(sock, outline,count + 1)<0)
+            printf("writing to server failed\n");
+        if(outline[0]=='0')
+            break;
+        printf("Client: sending word to server: %s",outline);
+        printf("\nClient: Write word %d to sort at server:\n", ++wordIndex);
+    }
+    printf("Client: results are...\n");
+    while ((count=read(sock,inlin,LINESIZE)) > 0) {
+        //printf("Client:");
+        write(1,inlin,count);
+        printf("\n");
+    }
+    return (0);
+}
---
+++ server.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define SORT_PORT 1719
+#define LINESIZE 80
+#define NUMWORDS 50
+
+void sort(char *str[],int n){
+	int i, j;
+	char temp[LINESIZE];
+	for(i = n; i >= 0; i--) {
+		for(j = 0; j < i - 1; j++) {
+			if(strcmp(str[j], str[j + 1]) > 0) {
+				strcpy(temp, str[j]);
+				strcpy(str[j], str[j + 1]);
+				strcpy(str[j + 1], temp);
+			}
+		}
+	}
+}
+
+void* func (void *s)		// function to send all the words
+{
+
+	int i = 0;
+	char buffer[LINESIZE];
+	char *words[NUMWORDS];
+	struct sockaddr_in server,client;
+
+	while(1)
+	{			//
+		int word_len = recv(*(int*)s, &buffer, LINESIZE, 0);
+		if (strcmp(buffer, "0\n") == 0) {
+			break;
+		} else {
+			words[i] = (char *) malloc(word_len);
+			strcpy(words[i], buffer);
+			printf("word added to words array: %s\n", buffer);
+			printf("word spot: %d\n", i);
+			i++;
+		}
+	}
+	sort(words, i);
+	int j;
+	for (j = 0; j < i; j++) {
+		int bytes_sent = send(*(int*)s, words[j], strlen(words[j]) + 1, 0);
+	}
+
+	close (*(int *) s);
+	free(s);
+
+}
+
+int main (int argc, char *argv[])
+{
+
+	struct sockaddr_in server,client;
+	int word_len, client_len, sockfdListen;
+
+	int *sockfd;
+	pthread_t thread; // creating storage
+
+	if((sockfdListen = socket(AF_INET,SOCK_STREAM,0)) < 0)   {
+		  perror("creating stream socket error!\n");
+		  exit(1);
+	}
+	server.sin_family=AF_INET;
+	server.sin_port=htons(SORT_PORT);
+	server.sin_addr.s_addr=htonl(INADDR_ANY);   
+
+	if(bind(sockfdListen,(struct sockaddr *)&server,sizeof(server)) < 0){
+		  perror("error in binding!");
+		  exit(1);
+	}
+	if(listen(sockfdListen,3)<0){
+		  perror("error in listening!");
+		  exit(1);
+	}
+	while(1)
+	{
+		client_len = sizeof(client);
+		sockfd = malloc(sizeof(int));
+		if((*sockfd=accept(sockfdListen,(struct sockaddr*)&client,&client_len))<0){
+		     perror("error in binding!");
+		     exit(1);
+		}
+		pthread_create(&thread, NULL, func, sockfd);
+
+	}
+	printf("\n\n");
+	exit(0);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
