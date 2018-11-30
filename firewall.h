19f60ab5859a5a8a4f9973c198d11d3bd9d4b1d4
gaitanOS <lgaitan@scu.edu>
2018-11-29 22:44:19 -0800

    Create firewall.h
    Header file to include in other files for firewall.c

---
+++ firewall.h
@@ -0,0 +1,17 @@
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <bool.h>
+/*
+firewall.c
+Function will accept the key passed through the socket from the SDN Controller and check if it is an authorized or unauthorized value.
+If it is authorized, it will return true.
+If it is unauthorized, it will return false.
+Parameters:
+key -> the random key generated in the application layer
+limit -> a defined value in the controller passed as a parameter. 
+It is generic so that it can change accordingly without affecting the code.
+*/
+
+bool firewall(void *key, int limit);
