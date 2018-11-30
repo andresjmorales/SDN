906483b279c85c1cfc5347debe1a68cd63bac55c
gaitanOS <lgaitan@scu.edu>
2018-11-29 22:54:32 -0800

    Update dijkstra.c
    Edited dijkstra.c to be used by calling one function.

--- dijkstra.c
+++ dijkstra.c
@@ -43,22 +43,24 @@ void predecessors(int P[], int index)
 }
 
 
-int dijkstra(int *table){
+int dijkstra(int *table, int dimension){
     
     /* ********************************************INITIALIZING VALUES*********************************************/
 
-    const int C[6][6] = {
+    /*const int C[6][6] = {
         {0, 7, 3, 5, 10000, 10000},
         {7, 0, 3, 10000, 4, 10000},
         {3, 3, 0, 4, 8, 10000},
         {5, 10000, 4, 0, 7, 9},
         {10000, 4, 8, 7, 0, 2},
         {10000, 10000, 10000, 9, 2, 0}
-    };
-    bool Np[6]; //N' Array to keep track of nodes that have the shortest path, basically sotre 0 or 1, true or false, if node is already in shortest path tree
-    int D[6], P[6]; //array to store the costs or distances between nodes, array to store predecessor nodes, source is 0 or u
+    };*/
+ 
+  
+    bool Np[dimension]; //N' Array to keep track of nodes that have the shortest path, basically sotre 0 or 1, true or false, if node is already in shortest path tree
+    int D[dimension], P[dimension]; //array to store the costs or distances between nodes, array to store predecessor nodes, source is 0 or u
     
-    for (int i = 0; i < 6; i++){
+    for (int i = 0; i < dimension; i++){
         P[i] = -1; //predecessor nodes set to -1
         if(i == 0){
             D[i] = 0;//cost to source node, u, should be 0
@@ -73,10 +75,10 @@ int dijkstra(int *table){
 
     
     /******************************************* Following the pseudocode****************************************/
-    for (int i = 0; i < 5; ++i){
+    for (int i = 0; i < dimension - 1; ++i){
         
         int initial_value = 10000, index = 0; //infinity is the initial value, the index should be set to 0 initially for the index of the minimum cost node
-        for (int i = 0; i < 6; ++i){
+        for (int i = 0; i < dimension; ++i){
             if (Np[i] == false && D[i] <= initial_value){//if the node is not in the shortest path tree and the cost is less than whatever the value currently in D is
                 initial_value = D[i]; //set the cost to the new or same lowest cost
                 index = i; //the index of the node is stored
@@ -85,10 +87,10 @@ int dijkstra(int *table){
         
         
         Np[index] = true; //the node is now in the shortest path tree
-        for (int j = 0; j < 6; ++j){
-            if (Np[j] == false && C[index][j] && D[index] + C[index][j] < D[j]){ //if it's not in the shortest path tree and the initial cost in C and the new cost d + c are less than what is in the cost array D
+        for (int j = 0; j < dimension; ++j){
+            if (Np[j] == false && table[index][j] && D[index] + table[index][j] < D[j]){ //if it's not in the shortest path tree and the initial cost in C and the new cost d + c are less than what is in the cost array D
                 P[j] = index; //set predecessor node
-                D[j] = D[index] + C[index][j]; //set new cost to get to node since multiple nodes were reached
+                D[j] = D[index] + table[index][j]; //set new cost to get to node since multiple nodes were reached
             }
         }
     }
@@ -97,37 +99,11 @@ int dijkstra(int *table){
 
     /******************************************* Printing the values ************************************************/
     
-    
-    char s = 'u'; //the source will be u for printing
-    for (int i = 1; i < 6; i++){
-        
-        //check each case to see which node is being looked at
-        if(i == 0){
-            printf("For nodes: %c to u\n The cost is %d\nThe predecessor list is: %c ", s, D[i], s);
-        }
-        else if(i == 1){
-            printf("For nodes: %c to v\n The cost is %d\nThe predecessor list is: %c ", s, D[i], s);
-        }
-        else if(i == 2){
-            printf("For nodes: %c to w\n The cost is %d\nThe predecessor list is: %c ", s, D[i], s);
-        }
-        else if(i == 3){
-            printf("For nodes: %c to x\n The cost is %d\nThe predecessor list is: %c ", s, D[i], s);
-        }
-        else if(i == 4){
-            printf("For nodes: %c to y\n The cost is %d\nThe predecessor list is: %c ", s, D[i], s);
-        }
-        else if(i == 5){
-            printf("For nodes: %c to z\n The cost is %d\nThe predecessor list is: %c ", s, D[i], s);
-        }
-        
-        predecessors(P, i); //function to make life easier to print out the predecessors for the nodes
-        printf("\n\n");
-    }
+   
 
     
     int d2[6];//temporary array used for comparison to store unsorted costs
-    for(int i = 0; i < 6; ++i){
+    for(int i = 0; i < dimension; ++i){
         
         d2[i] = D[i]; //store costs before sorting
         
@@ -137,33 +113,22 @@ int dijkstra(int *table){
     
     printf("Shortest Path Tree Based On Distance: ");
     
-    for(int i=0; i<6; ++i){
-        for(int j =0; j<6; ++j){
+    for(int i=0; i< dimension; ++i){
+        for(int j =0; j< dimension; ++j){
             /*
              Logic: compare unsorted and sorted arrays... the D cost array is ordered by index, where the index represents a node. example: 0 is u, 1 is v, etc. check the unsorted list for a matching cost. when a match is found, we know what letter to print based off of the index of the unsorted list.
              
              */
             if(d2[j] == D[i]){
-                
-                if(j == 0){
-                    printf("u ");
-                }
-                else if(j == 1){
-                    printf("v ");
-                }
-                else if(j == 2){
-                    printf("w ");
-                }
-                else if(j == 3){
-                    printf("x ");
-                }
-                else if(j == 4){
-                    printf("y ");
-                }
-                else if(j == 5){
-                    printf("z \n");
-                }
-            }
+               
+              if(i == (dimension - 1)){
+               
+                printf("Device %d\n", i);
+               
+              }
+              else{
+                printf("Device %d --> ", i);
+              }
         }
     }
     
