489eabdc2cfd00a96ef129d1a80daa7668f3d62a
gaitanOS <lgaitan@scu.edu>
2018-12-05 12:55:50 -0800

    Update controller.c
    Modified to implement a check. If 0, then the program will stop. If 1, then the program will continue to send an authenticated or unauthenticated value.

--- controller-device/controller.c
+++ controller-device/controller.c
@@ -20,8 +20,9 @@ int main(int argc, char* argv[]) {
 	}
 
 	// Initialize variables.
-	int sockfd, addr_len, bytes_read;
+	int sockfd, addr_len, bytes_read, check = 1; //check is the stop case if something malicious is detected
 	char recv_data[1000];
+	char authStore[10];
 	struct sockaddr_in server_addr , client_addr;
 
 	// Error with creation of socket.
@@ -46,13 +47,26 @@ int main(int argc, char* argv[]) {
 	printf("UDPServer Waiting for client on port 5000...\n");
 	fflush(stdout);
 	// Server stays open until program terminated.
-	while (1) {
+	while (check != 0) {
+		
+		/******APPLICATON******/
+		int len = 0; //number of digits
+		int gen = auth(LIMIT, len); //stores generated value from auth
+		//authStore[0] = '';
+		sprintf(authStore, "%d", gen);
+		//authStore[0] = (char)gen;
+		
+		/******ENDAPPLICATION******/
+		
+		sendto(sockfd, authStore, len, 0, (struct sockaddr *)&client_addr, sizeof(struct sockaddr)); //route or send value from auth to device
+		
+		
 		// Read bytes from socket.
-		bytes_read = recvfrom(sockfd,recv_data,1024,0,
-						   (struct sockaddr *)&client_addr, &addr_len);
+		bytes_read = recvfrom(sockfd,recv_data,1024,0, (struct sockaddr *)&client_addr, &addr_len);
 		// Add null terminating character to string created.
 		recv_data[bytes_read] = '\0';
-
+		check = atoi(recv_data);
+		printf("Controller has received %d from device.", check);
 		// Write recieved characters to the specified file.
 		//FILE * ofp = fopen(argv[1], "w");
 		
@@ -65,10 +79,9 @@ int main(int argc, char* argv[]) {
 		//fclose(ofp);
 
 		// Display characters read (for confirmation).
-		printf("\n(%s , %d) said : ",inet_ntoa(client_addr.sin_addr),
-						                  ntohs(client_addr.sin_port));
-		printf("%s", recv_data);
-		fflush(stdout);
+		//printf("\n(%s , %d) said : ",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+		//printf("%s", recv_data);
+		//fflush(stdout);
 	} 
 
 }
