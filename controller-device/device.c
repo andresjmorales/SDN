6a0dfe2ac778bef6a1dfedd8647ca3be79aeab74
moralesandres98 <morales.andres98@gmail.com>
2018-12-05 12:26:12 -0800

    Update device.c
    updated socket programming, plan for authentication

--- controller-device/device.c
+++ controller-device/device.c
@@ -11,7 +11,8 @@
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2) {
+	int ip_ind = 2;
+	if (argc < ip_ind) {
 		printf("\n\tusage: ./device file.txt [ip]\n");
 		return 1;
 	}
@@ -19,7 +20,7 @@ int main(int argc, char* argv[])
     int sockfd;
     struct sockaddr_in server_addr;
     char send_data[1024];
-    struct hostent *host = (struct hostent *)gethostbyname((argc > 2) ? argv[2] : "localhost");
+    struct hostent *host = (struct hostent *)gethostbyname((argc > ip_ind) ? argv[ip_ind] : "localhost");
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         perror("socket");
         exit(1);
@@ -28,18 +29,20 @@ int main(int argc, char* argv[])
     server_addr.sin_port = htons(5000);
     server_addr.sin_addr = *((struct in_addr *)host->h_addr);
     bzero(&(server_addr.sin_zero), 8);
-    
-
+	
 /*
-	// send file
 	FILE *f;
-    f = fopen(argv[1],"r");		// open file given in terminal
-	// read data from file
+    f = fopen(argv[1],"r");		// open file given in terminal, read
 	int bytes_read;
-    while (!feof(f)) {
+    while (!feof(f))
         bytes_read = fread(&send_data,1,sizeof(send_data),f);
-    }
 */
+	
+	// authentication
+	// receive authentication number
+	// runs firewall on that authentication number
+	// disconnect if blocked by firewall
+	
 	sendto(sockfd, send_data, bytes_read, 0, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
     fclose(f);
 
